0b0ba38f193809b228c433e20c562006d89b19c2
Michael Gurley <m-gurley@northwestern.edu>
2017-01-23 08:36:40 -0500

    references #25 Prevent deletion of specimen types if request details exist.
    Errors are not gracefully showing up on the page.  Need to look into it.

--- app/models/specimen_type.rb
+++ app/models/specimen_type.rb
@@ -1,5 +1,6 @@
 class SpecimenType < ApplicationRecord
   belongs_to :repository
+  has_many :disburser_request_details, dependent: :restrict_with_error
 
   validates_presence_of :name
 end
--- spec/models/specimen_type_spec.rb
+++ spec/models/specimen_type_spec.rb
@@ -3,5 +3,6 @@ require 'active_support'
 
 RSpec.describe SpecimenType, type: :model do
   it { should belong_to :repository }
+  it { should have_many :disburser_request_details }
   it { should validate_presence_of :name }
 end
