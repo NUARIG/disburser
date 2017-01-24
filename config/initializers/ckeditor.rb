c11b57ade1dc326d4c730c9d69d8c0b52c89294e
Michael Gurley <m-gurley@northwestern.edu>
2017-01-24 09:28:06 -0500

    references #10 Allow all the plugins in the CDN version of the creditor

--- config/initializers/ckeditor.rb
+++ config/initializers/ckeditor.rb
@@ -1,4 +1,5 @@
 Ckeditor.setup do |config|
+  # http://cdn.ckeditor.com/
   # //cdn.ckeditor.com/<version.number>/<distribution>/ckeditor.js
-  config.cdn_url = "//cdn.ckeditor.com/4.6.1/basic/ckeditor.js"
+  config.cdn_url = "//cdn.ckeditor.com/4.6.1/full-all/ckeditor.js"
 end
