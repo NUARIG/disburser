d1e0a3d2380a7098597c4aef1705b1f03b121034
Michael Gurley <m-gurley@northwestern.edu>
2017-02-13 12:30:39 -0500

    references #48 Change references to enosis in monit scripts.

--- config/monit/production/delayed_job_start.sh
+++ config/monit/production/delayed_job_start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-cd /var/www/apps/enotis/current
-RAILS_ENV=production /var/www/apps/enotis/current/bin/delayed_job --pid-dir=/var/www/apps/enotis/shared/pids/ start
+cd /var/www/apps/disburser/current
+RAILS_ENV=production /var/www/apps/disburser/current/bin/delayed_job --pid-dir=/var/www/apps/disburser/shared/pids/ start
