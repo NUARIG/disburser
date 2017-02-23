8f2c8df86ea416aa2a7c4210258986d1e6601c44
Michael Gurley <m-gurley@northwestern.edu>
2017-02-23 16:18:50 -0500

    references #70 Fix typo in migration.

--- db/migrate/20170222215609_change_columns_nullable_disburser_requests.rb
+++ db/migrate/20170222215609_change_columns_nullable_disburser_requests.rb
@@ -2,6 +2,6 @@ class ChangeColumnsNullableDisburserRequests < ActiveRecord::Migration[5.0]
   def change
     change_column_null(:disburser_requests, :methods_justifications, true)
     change_column_null(:disburser_requests, :cohort_criteria, true)
-    change_column_null(:disburser_requestss, :data_for_cohort, true)
+    change_column_null(:disburser_requests, :data_for_cohort, true)
   end
 end
