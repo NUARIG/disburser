41e7630f485b18d917362cf7c972de1ca7ff211a
Michael Gurley <m-gurley@northwestern.edu>
2017-02-27 09:42:44 -0500

    references #11 Change MONIT scripts to play nice with RVM in production.

--- config/monit/production/delayed_job_stop.sh
+++ config/monit/production/delayed_job_stop.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 cd /var/www/apps/disburser/current
-RAILS_ENV=production /var/www/apps/disburser/current/bin/delayed_job --pid-dir=/var/www/apps/disburser/shared/pids/ stop
+/usr/local/rvm/bin/rvm-shell -c 'RAILS_ENV=production /var/www/apps/disburser/current/bin/delayed_job --pid-dir=/var/www/apps/disburser/shared/pids/ stop'
